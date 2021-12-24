81a40bef3b6965315730babfab6277f0396a15ea
Sdy <Sdy@qq.com>
2021-12-24 17:18:28 +0800

    second commit

--- src/main/java/com/sdy/git/GigTest.java
+++ src/main/java/com/sdy/git/GigTest.java
@@ -3,5 +3,6 @@ package com.sdy.git;
 public class GigTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
